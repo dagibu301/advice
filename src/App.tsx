1791c39716ce84b55ed4c349bc4b4f294ac52737
David Giraldo <davidgiraldobueno@gmail.com>
2020-11-21 12:06:28 -0500

    get advice

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    advice: ''
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then(response => {
+      console.log(response.data.slip.advice);
+    })
+    .catch(error => { 
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <h1>App</h1>
+    )
+  }
 }
 
 export default App;
